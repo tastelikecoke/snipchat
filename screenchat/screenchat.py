a62976f80156e18da790dd656d22d1c7919b5fe5
tastelikecoke <jmerc87@gmail.com>
2026-03-13 23:27:54 +0800

    First commit

---
+++ screenchat/screenchat.py
@@ -0,0 +1,28 @@
+import pyautogui
+from ollama import chat
+from ollama import ChatResponse
+import time
+import pydirectinput
+# Sleep for 2 seconds
+
+
+while True:
+    question = input("what is your question?")
+    print("Go to the window you want to show.")
+    time.sleep(5)
+    print("checking screen.")
+    im1 = pyautogui.screenshot('./my_screenshot1.png')
+
+    response: ChatResponse = chat(model="qwen3-vl:2b", messages=[
+        {
+            "role": "user",
+            "content": question,
+            "images": [
+                "./my_screenshot1.png"
+            ]
+        }
+    ])
+
+    print(response.message.content)
+    print("Hope this response satisfies you.")
+    time.sleep(2)
